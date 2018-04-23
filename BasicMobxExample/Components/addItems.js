04b613df0fbbef10bd36317277e2914b1d48e2fc
Gant <GantMan@gmail.com>
2018-04-22 23:58:47 -0500

    :hocho: cut down on LOC

--- BasicMobxExample/Components/addItems.js
+++ BasicMobxExample/Components/addItems.js
@@ -1,53 +1,18 @@
 import React, { Component } from 'react'
-import {
-  Platform,
-  StyleSheet,
-  TextInput,
-  Text,
-  View,
-  TouchableOpacity
-} from 'react-native'
-
+import { AddPackingItem } from 'packlist-components'
 import ListStore from '../Mobx/listStore'
 import {observer} from 'mobx-react/native'
 
 @observer
 export default class AddItems extends Component {
-
-  render() {
+  render () {
     return (
-      <View>
-        <TextInput placeholder='Packing Item'
-          autoFocus
-          blurOnSubmit
-          clearTextOnFocus
-          onSubmitEditing={ListStore.addItem}
-          onChangeText={ListStore.setNewItemName}
-          value={ListStore.newItemName}
-          returnKeyLabel='Add Item'
-          returnKeyType='done'
-        />
-        <TouchableOpacity
-          style={[{backgroundColor: 'green'}, styles.button]}
-          onPress={ListStore.addItem}>
-          <Text>ADD ITEM</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[{backgroundColor: 'red'}, styles.button]}
-          onPress={ListStore.clear}>
-          <Text>CLEAR ITEMS</Text>
-        </TouchableOpacity>
-      </View>
+      <AddPackingItem
+        addItem={ListStore.addItem}
+        setNewItemText={ListStore.setNewItemName}
+        value={ListStore.newItemName}
+        clear={ListStore.clear}
+      />
     )
   }
 }
-
-const styles = StyleSheet.create({
-  button: {
-    alignSelf: 'center',
-    borderRadius: 4,
-    margin: 5,
-    padding: 10
-  }
-})
-
--- BasicMobxExample/Components/listItems.js
+++ BasicMobxExample/Components/listItems.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react'
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  LayoutAnimation
-} from 'react-native'
+import { SimpleList } from 'packlist-components'
 
 import ListStore from '../Mobx/listStore'
 import {observer} from 'mobx-react/native'
 
 @observer
 export default class ListItems extends Component {
-
-  listItems = () => {
-    // window.alert(this.props.allItems)
-    LayoutAnimation.spring()
-    return ListStore.allItems.map(item => <Text key={item}>{item}</Text>)
-  }
-
-  render() {
+  render () {
     return (
-        <View>
-          { this.listItems() }
-        </View>
+      <SimpleList
+        value={[...ListStore.allItems]}
+      />
     )
   }
 }
