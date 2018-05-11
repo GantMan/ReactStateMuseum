e599247346e4220695a7d311e4b14cf2ba7f8a4b
Juan David Castro <me@juandc.co>
2018-05-11 14:14:03 -0500

    :lipstick: Added Styles

--- React/unistore/src/index.js
+++ React/unistore/src/index.js
@@ -11,19 +11,65 @@ import { render } from "react-dom";
 import { Provider, connect } from "unistore/react";
 import { store, actions } from "./store";
 
+const styles = {
+  div: {
+    fontFamily: "sans-serif",
+    textAlign: "center"
+  },
+  ul: {
+    listStyle: "none",
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    padding: 0
+  },
+  li: {
+    borderBottom: "1px solid blue",
+    display: "block",
+    padding: "7.5px 2.5px 5px",
+    width: "100px",
+    margin: "0 30%"
+  },
+  input: {
+    backgroundColor: "#7a7eff",
+    padding: "7.5px"
+  },
+  addButton: {
+    backgroundColor: "#ccfecc",
+    borderRadius: "4px",
+    margin: "2px 5px",
+    padding: "7.5px"
+  },
+  cleanButton: {
+    backgroundColor: "#feaaaa",
+    borderRadius: "4px",
+    margin: "2px 5px",
+    padding: "7.5px"
+  }
+};
+
 const App = connect("allItems", actions)(
   ({ allItems, addItem, clearItems, newItem, setNewItemValue }) => (
-    <div>
+    <div style={styles.div}>
       <h2>Welcome to Unistore!</h2>
       <input
         type="text"
+        style={styles.input}
         value={newItem}
         onChange={e => setNewItemValue({ value: e.target.value })}
       />
-      <button onClick={addItem}>Add Item</button>
-      <button onClick={clearItems}>Clear Items</button>
-      <ul>
-        {allItems.map((item, index) => <li key={item + index}>{item}</li>)}
+      <button style={styles.addButton} onClick={addItem}>
+        Add Item
+      </button>
+      <button style={styles.cleanButton} onClick={clearItems}>
+        Clear Items
+      </button>
+      <ul style={styles.ul}>
+        {allItems.map((item, index) => (
+          <li key={item + index} style={styles.li}>
+            {item}
+          </li>
+        ))}
       </ul>
     </div>
   )
