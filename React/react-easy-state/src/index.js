97b848529cf0b16de1f56fb230141a6f76577e06
Juan David Castro <me@juandc.co>
2018-05-11 12:05:08 -0500

    :tada: Initial commit

---
+++ React/react-easy-state/src/index.js
@@ -0,0 +1,18 @@
+/************************************************
+ * This example is one of many examples
+ *         -= The React State Museum =-
+ * View other state management options and read
+ * more in the blog post and the master repo:
+ *
+ * https://github.com/GantMan/ReactStateMuseum
+ ************************************************/
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+const App = () => (
+  <div>
+    <h2>Hello world!</h2>
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
