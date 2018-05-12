2c1dc26acf01ecd4cface4cdad1b35df0f8b629e
Iurii Kyrylenko <yuriproject@gmail.com>
2018-05-12 18:32:38 +0300

    :bento: redux: excluded extra containers (#32)

--- React/redux/src/Components/addItem.js
+++ React/redux/src/Components/addItem.js
@@ -1,27 +1,15 @@
-import React, { Component } from "react";
 import { AddPackingItem } from "packlist-components";
 import { ItemActionCreators } from "../Redux/Actions/items";
 import { connect } from "react-redux";
 
-class AddItems extends Component {
-  render() {
-    return (
-      <AddPackingItem
-        addItem={() => this.props.dispatch(ItemActionCreators.addItem())}
-        setNewItemText={e =>
-          this.props.dispatch(ItemActionCreators.setNewItemName(e.target.value))
-        }
-        value={this.props.newItemName}
-        clear={() => this.props.dispatch(ItemActionCreators.clear())}
-      />
-    );
-  }
-}
+const mapStateToProps = state => ({ newItemName: state.items.newItemName });
 
-const mapStateToProps = state => {
-  return {
-    newItemName: state.items.newItemName
-  };
+const { setNewItemName, addItem, clear } = ItemActionCreators;
+
+const mapDispathToProps = {
+  setNewItemText: e => setNewItemName(e.target.value),
+  addItem,
+  clear
 };
 
-export default connect(mapStateToProps)(AddItems);
+export default connect(mapStateToProps, mapDispathToProps)(AddPackingItem);
