b462457786fd17fa32ff7761552fc85919bc22e5
Juan David Castro <me@juandc.co>
2018-05-17 08:44:44 -0500

    :sparkles: Example working

--- React/rematch/src/index.js
+++ React/rematch/src/index.js
@@ -8,9 +8,45 @@
  ************************************************/
 import React from "react";
 import { render } from "react-dom";
+import { Provider, connect } from "react-redux";
 import ListItems from "./Components/listItems";
 import AddItems from "./Components/addItem";
+import store from "./store";
 
-const App = () => <div>Hello World!</div>;
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
 
-render(<App />, document.getElementById("root"));
+const Items = ({ allItems, newItem, addItem, clearItems, setNewItemText }) => (
+  <div style={styles}>
+    <h2>Welcome to React Easy State!</h2>
+    <AddItems
+      addItem={addItem}
+      setNewItemText={event => setNewItemText(event.target.value)}
+      value={newItem}
+      clear={clearItems}
+    />
+    <ListItems allItems={allItems} />
+  </div>
+);
+
+const mapState = models => ({
+  allItems: models.items.allItems,
+  newItem: models.items.newItem
+});
+
+const mapDispatch = models => ({
+  addItem: models.items.addItem,
+  clearItems: models.items.clearItems,
+  setNewItemText: models.items.setNewItemText
+});
+
+const App = connect(mapState, mapDispatch)(Items);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ React/rematch/src/models.js
@@ -0,0 +1,20 @@
+export const items = {
+  state: {
+    newItem: "",
+    allItems: ["Hot Dog", "Pizza", "Donuts"]
+  },
+  reducers: {
+    setNewItemText: (state, payload) => ({
+      ...state,
+      newItem: payload
+    }),
+    addItem: state => ({
+      ...state,
+      allItems: state.allItems.concat([state.newItem])
+    }),
+    clearItems: state => ({
+      ...state,
+      allItems: []
+    })
+  }
+};
