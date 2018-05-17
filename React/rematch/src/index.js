0edbac836d4a82bb07877d1f67f0a72377899023
Juan David Castro <me@juandc.co>
2018-05-17 08:19:40 -0500

    :tada: Initial commit

---
+++ React/rematch/src/index.js
@@ -0,0 +1,16 @@
+/************************************************
+ * This example is one of many examples
+ *         -= The React State Museum =-
+ * View other state management options and read
+ * more in the blog post and the master repo:
+ *
+ * https://github.com/GantMan/ReactStateMuseum
+ ************************************************/
+import React from "react";
+import { render } from "react-dom";
+import ListItems from "./Components/listItems";
+import AddItems from "./Components/addItem";
+
+const App = () => <div>Hello World!</div>;
+
+render(<App />, document.getElementById("root"));
