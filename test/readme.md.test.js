d6fa60cdf272153c87ec761d38eb4e5be145f9df
Michael Stecky-Efantis <m.stecky.efantis@gmail.com>
2018-05-19 15:19:54 -0400

    added basic test framework

---
+++ test/readme.md.test.js
@@ -0,0 +1,15 @@
+'use strict';
+/* jshint expr: true */
+
+const { expect } = require( 'chai' );
+
+
+describe( 'README.MD Tests', function() {
+
+    it( 'Making sure that each folder has been mentioned in the readme', function() {
+
+        expect( 2 + 2 ).to.equal( 4 );
+
+        // TODO: add test code here
+    });
+});
