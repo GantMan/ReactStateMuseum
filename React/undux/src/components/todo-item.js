115069d745a7996837aa2789f5819b3a401246f0
Ryan <chimon2000@users.noreply.github.com>
2018-10-23 01:42:22 -0400

    🦆 Added undux (react) (#108)
    * Added undux (react)
    
    Resolves #107
    
    * Modified preset to be in dev dependencies
    
    * Added README.md
    
    * Added keywords and removed private
    
    * Added link to README

---
+++ React/undux/src/components/todo-item.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { List, Input, Button, Icon, Base } from 'usthing-ui'
+import TodoContainer from './todo-container'
+
+const TodoItem = ({ index, todo, removeTodo, updateTodo, editing, setEditing }) => (
+    <List.Item display="flex" alignItems="center">
+        <Base flex="1">
+            {editing[index] ? <Input value={todo} onChange={e => updateTodo(e.target.value, index)} /> : todo}
+        </Base>
+        <Button onClick={() => setEditing(index)} circle plain>
+            <Icon name={editing[index] ? 'eye' : 'edit-2'} />
+        </Button>
+        <Button onClick={() => removeTodo(index)} circle plain>
+            <Icon color="red" name="minus" />
+        </Button>
+    </List.Item>
+)
+
+export default TodoItem
